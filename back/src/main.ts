152ac9cd83c09b64502ad0a0fa21746ce9aaf140
axelirm <axelirm@hotmail.com>
2024-04-09 03:09:15 -0600

    update cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
+    credentials: true
   });
   await app.listen(3000);
 }
