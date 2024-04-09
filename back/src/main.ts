8e8d5bb98e2a8b1dd206ca92bf0912fb1da94068
axelirm <axelirm@hotmail.com>
2024-04-09 02:43:17 -0600

    added cors origin

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: 'https://gaiaalimentos.vercel.app/'});
+  app.enableCors({
+    origin: ['https://gaiaalimentos.vercel.app/ventas', 'https://gaiaalimentos.vercel.app/ordena-aqui']
+  });
   await app.listen(3000);
 }
 bootstrap();
