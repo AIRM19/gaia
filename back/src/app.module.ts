b7f8fb3383acda0449c76d24787bf0055779d4c2
axelirm <axelirm@hotmail.com>
2024-04-09 01:17:05 -0600

    service added to provider

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,10 +3,11 @@ import { ProductoModule } from './producto/producto.module';
 import { OpcionModule } from './opcion/opcion.module';
 import { PedidoModule } from './pedido/pedido.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), ProductoModule, OpcionModule, PedidoModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
